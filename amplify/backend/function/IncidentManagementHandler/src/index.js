5e9efe1e5f6d477cc5ee9f76c57cfbce8fc66e5a
Gabriel Gonzalez <hgonzalez.leis@gmail.com>
2022-08-12 08:54:14 -0600

    chore: amplify

--- amplify/backend/function/IncidentManagementHandler/src/index.js
+++ amplify/backend/function/IncidentManagementHandler/src/index.js
@@ -1,19 +1,15 @@
+const awsServerlessExpress = require('aws-serverless-express');
+const app = require('./app');
 
+/**
+ * @type {import('http').Server}
+ */
+const server = awsServerlessExpress.createServer(app);
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
-exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    const customerId = event.pathParameters.customerId;
-    const customer = { customerId: customerId, customerName: `Customer ${customerId}` };
-    return {
-      statusCode: 200,
-      //  Uncomment below to enable CORS requests
-      //  headers: {
-      //      "Access-Control-Allow-Origin": "*",
-      //      "Access-Control-Allow-Headers": "*"
-      //  },
-      body: JSON.stringify(customer),
-    };
+exports.handler = (event, context) => {
+  console.log(`EVENT: ${JSON.stringify(event)}`);
+  return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
 };
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,17 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-    "function": {
-        "IncidentManagementHandler": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        }
-    },
-    "api": {
-        "api1b437596": {
-            "RootUrl": "string",
-            "ApiName": "string",
-            "ApiId": "string"
-        }
-    }
-}
+export type AmplifyDependentResourcesAttributes = {}
