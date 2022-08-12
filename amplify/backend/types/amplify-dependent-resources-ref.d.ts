ca7bbcc763a6b5215971f64b73b038f321277c65
Gabriel Gonzalez <hgonzalez.leis@gmail.com>
2022-08-12 07:36:03 -0600

    chore: adding rest api backend to the app

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "IncidentManagementHandler": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "api1b437596": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
