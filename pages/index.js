66432f08f364311655fe1b7c6708e25f86537159
Gabriel Gonzalez <hgonzalez.leis@gmail.com>
2022-08-12 09:11:21 -0600

    chore: someth

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import { Amplify, API } from 'aws-amplify';
 import styles from '../styles/index.module.css';
 import { useState, useEffect } from 'react';
+import { TextField } from '@aws-amplify/ui-react';
 
 export default function Home() {
   const [incidentManagement, setIncidentManagement] = useState([]);
+  const [data, setData] = useState({});
+
+  const onChange = (value) => {
+    setData({ ...data, whenItHappen: value });
+  };
 
   const myAPI = 'api1b437596';
   const path = '/incidentManagement';
@@ -28,7 +34,14 @@ export default function Home() {
     <div className={styles.container}>
       <h1>Incident Management System</h1>
 
-      <form></form>
+      <form>
+        <TextField
+          descriptiveText="When did the incident happen"
+          placeholder="...."
+          label="When It Happen"
+          onChange={(evt) => onChange(evt.target.value)}
+        />
+      </form>
     </div>
   );
 }
