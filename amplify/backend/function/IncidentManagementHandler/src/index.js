0e633652884c2673eb207cda5246e30aa7a69c24
Gabriel Gonzalez <hgonzalez.leis@gmail.com>
2022-08-12 08:10:16 -0600

    setting up

--- amplify/backend/function/IncidentManagementHandler/src/index.js
+++ amplify/backend/function/IncidentManagementHandler/src/index.js
@@ -5,13 +5,15 @@
  */
 exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
+    const customerId = event.pathParameters.customerId;
+    const customer = { customerId: customerId, customerName: `Customer ${customerId}` };
     return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
+      statusCode: 200,
+      //  Uncomment below to enable CORS requests
+      //  headers: {
+      //      "Access-Control-Allow-Origin": "*",
+      //      "Access-Control-Allow-Headers": "*"
+      //  },
+      body: JSON.stringify(customer),
     };
 };
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,31 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { Amplify, API } from 'aws-amplify';
 import styles from '../styles/index.module.css';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [incidentManagement, setIncidentManagement] = useState([]);
+
+  const myAPI = 'api1b437596';
+  const path = '/incidentManagement';
+
+  useEffect(() => {
+    const getCustomer = async () => {
+      API.get(api, path + '/')
+        .then((response) => {
+          console.log(response);
+          let newCustomers = [...customers];
+          newCustomers.push(response);
+          setCustomers(newCustomers);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    getCustomer();
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -17,8 +40,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Get started by editing <code className={styles.code}>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
@@ -32,10 +54,7 @@ export default function Home() {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
+          <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
             <h2>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
@@ -45,9 +64,7 @@ export default function Home() {
             className={styles.card}
           >
             <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
       </main>
@@ -65,5 +82,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
